e7ebc1e7c79cdd2ac67a1307573bfacc39e675db
Patrick McFadden <pmcfadde@thoughtworks.com>
2022-08-05 12:44:55 -0600

    [pmcfadden] Adding colima as a possible context for local dev

--- tasks/shared.py
+++ tasks/shared.py
@@ -3,12 +3,13 @@ import os, sys
 
 CONTEXT_MINIKUBE="minikube"
 CONTEXT_DOCKER_FOR_MAC="docker-for-mac"
+CONTEXT_COLIMA="colima"
 cmd="kubectl config current-context"
 
 def is_local():
     """sys.exit if not using current-context of minikube or docker-for-mac"""
     result = os.popen(cmd).read().rstrip()
-    if result!=CONTEXT_MINIKUBE and result!=CONTEXT_DOCKER_FOR_MAC:
+    if result not in [CONTEXT_COLIMA, CONTEXT_DOCKER_FOR_MAC, CONTEXT_MINIKUBE]:
       #print('err: these shortcuts for local kubernetes only')
       sys.exit('err: these shortcuts for local kubernetes only')
     return True
